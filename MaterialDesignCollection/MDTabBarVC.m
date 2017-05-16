fd612fea3d277a12f1833af70bfb9e5fc6c74e2d
MHZ <wlsxmahaozhe@163.com>
2017-05-16 20:18:53 +0800

    添加分栏点击弹动效果

--- MaterialDesignCollection/MDTabBarVC.m
+++ MaterialDesignCollection/MDTabBarVC.m
@@ -38,6 +38,15 @@
 
 @implementation MDTabBarVC
 
+
+-(void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    [self layoutSubviews];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
